0c2bfb909b206ed01b588dc0fbcec9e213f37dfd
AndressaSL <santoslima.andressa@gmail.com>
2022-08-17 02:29:13 -0300

    Exercícios da Aula - Entendendo Métodos Java

---
+++ MetodosJava/src/ExecutarMetodos.java
@@ -0,0 +1,29 @@
+public class ExecutarMetodos {
+
+    public static void main(String[] args) {
+
+        // Calculadora
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        System.out.println("------------------------------------------------------------------------------------------------------");
+
+        // Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        System.out.println("------------------------------------------------------------------------------------------------------");
+
+        // Emprestimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+    }
+}
